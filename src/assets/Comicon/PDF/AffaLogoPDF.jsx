49f019b7ef156c930c1207db022f4b1d5852b58a
Andrew Sharanda <azzimandias@gmail.com>
2025-09-18 11:22:23 +0300

    bid page update

---
+++ src/assets/Comicon/PDF/AffaLogoPDF.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AffaImg from './media/Affa_logo.png';
+const AffaLogoPDF = () => {
+    return (
+        <img src={AffaImg}
+             alt="affa"
+             style={{marginRight: '10px', marginTop: '2mm', height: '10mm'}}
+        />
+    );
+};
+
+export default AffaLogoPDF;
---
+++ src/assets/Comicon/PDF/PMU60N.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import pmu60n from "./media/pmu60n.png";
+
+const Pmu60N = () => {
+    return (
+        <img src={pmu60n}
+             className="start-photo"
+             alt="Pmu60N"
+        />
+    );
+};
+
+export default Pmu60N;
