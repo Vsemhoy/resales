e15486d3909c342137f2d96c26841f827a7321e2
Semenov Demian <d.semenov@arstel.com>
2025-09-25 12:03:10 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { App } from './App';
 import reportWebVitals from './reportWebVitals';
 import { ConfigProvider } from 'antd';
 
 /* + LOCALE */
 import ruRU from 'antd/lib/locale/ru_RU'; // Импорт русской локали
-import ru from 'antd/es/date-picker/locale/ru_RU';
 
 const customLocale = {
 	...ruRU,
