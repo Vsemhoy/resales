59bbefe9653d32cb0f01142a9ba6a888fbbc1195
Andrew Sharanda <azzimandias@gmail.com>
2025-09-29 13:12:02 +0300

    bid page update

--- src/modules/PRICE/Price.jsx
+++ src/modules/PRICE/Price.jsx
@@ -5,7 +5,7 @@ import { PRODMODE, CSRF_TOKEN } from '../../config/config';
 import dayjs from 'dayjs';
 import './components/style/orgpage.css';
 
-import { Tree, Button, Spin, message, Table, Tag, Switch, Divider, Checkbox } from 'antd';
+import {Tree, Button, Spin, message, Table, Tag, Switch, Divider, Checkbox, Layout, Affix} from 'antd';
 import axios from 'axios';
 import * as XLSX from 'xlsx';
 
@@ -14,160 +14,182 @@ import { PROD_AXIOS_INSTANCE } from '../../config/Api';
 import {DS_CURRENCY, PRICE} from './mock/mock';
 
 import './style/price_style.css'
+import CurrencyMonitorBar from "../../components/template/CURRENCYMONITOR/CurrencyMonitorBar";
+import {FilterOutlined} from "@ant-design/icons";
+import BidListSiderFilters from "../BID_LIST/components/BidListSiderFilters";
+import Sider from "antd/es/layout/Sider";
+import {Content} from "antd/es/layout/layout";
 
 const Price = () => {
-	const defaultCheckedList = ['Предпродажа', 'Проектная', 'Прайс 20', 'Прайс 30'];
-	const [checkedList, setCheckedList] = useState(defaultCheckedList);
 	const [currency, setCurrency] = useState(PRODMODE ? null : DS_CURRENCY);
 	const [treeData, setTreeData] = useState([]);
 	const [checkedKeys, setCheckedKeys] = useState([]);
-	const [loading, setLoading] = useState(PRODMODE);
+	const [loading, setLoading] = useState(false);
 	const [currentCurrency, setCurrentCurrency] = useState(false);
-	const CheckboxGroup = Checkbox.Group;
-	const plainOptions = ['Предпродажа', 'Проектная', 'Прайс 20', 'Прайс 30'];
-	const indeterminate = checkedList.length > 0 && checkedList.length < plainOptions.length;
-	const checkAll = plainOptions.length === checkedList.length;
-	const onChange = (list) => {
-		setCheckedList(list);
-	};
+
+	const [isOpenedFilters, setIsOpenedFilters] = useState(true);
+	const defaultCheckedList = [
+		{
+			name: 'Розница',
+			checked: true,
+		},
+		{
+			name: 'Прайс 10',
+			checked: true,
+		},
+		{
+			name: 'Прайс 20',
+			checked: true,
+		},
+		{
+			name: 'Прайс 30',
+			checked: true,
+		},
+	];
+	const [checkedList, setCheckedList] = useState(defaultCheckedList);
 	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		fetchFields().then();
+		fetchCurrency().then();
+		setCheckedList(defaultCheckedList);
+	}, []);
+	useEffect(() => {
+		const treeNodes = mapDataToTreeNodes(data);
+		setTreeData(treeNodes);
+	}, [data, checkedList, currentCurrency]);
+
+	const fetchFields = async () => {
+		if (PRODMODE) {
+			const format_data = {
+				_token: CSRF_TOKEN,
+			};
+			try {
+				setLoading(true);
+				let fields_req = await PROD_AXIOS_INSTANCE.post(`api/sales/price`, format_data);
+				if (fields_req) {
+					setData(fields_req.data.data[0].childs);
+				}
+			} catch (e) {
+				console.log(e);
+			} finally {
+				setLoading(false);
+			}
+		} else {
+			setData(PRICE.childs);
+		}
+	};
+	const fetchCurrency = async () => {
+		if (PRODMODE) {
+			try {
+				setLoading(true);
+				const format_data = {
+					_token: CSRF_TOKEN,
+					data: {},
+				};
+				const currency_response = await PROD_AXIOS_INSTANCE.post(
+					`api/currency/getcurrency`,
+					format_data
+				);
+				if (currency_response) {
+					setCurrency(currency_response.data);
+				}
+			} catch (e) {
+				console.log(e);
+			} finally {
+				setLoading(false);
+			}
+		}
+	};
+
+	const onChangeCheckbox = (checked, idx) => {
+		const checkedListUpd = JSON.parse(JSON.stringify(checkedList));
+		checkedListUpd[idx].checked = checked;
+		setCheckedList(checkedListUpd);
+	};
 	const convertToRub = (price, curr) => price * currency.company[curr].value;
-	const getCurrencySimbol = (c) => {
+	const getCurrencySymbol = (c) => {
 		return currentCurrency ? '₽' : c === 0 ? '$' : '€';
 	};
-	function mapDataToTreeNodes(data, parentKey = '') {
-		const columns = [
-			{
-				key: 'name',
-				dataIndex: 'name',
-				width: 60,
-				render: (e) => <div style={{ minWidth: '60px' }}>{e}</div>,
-			},
-			{
-				key: 'descr',
-				dataIndex: 'descr',
-				render: (e) => <div style={{ minWidth: '400px' }}>{e}</div>,
-				width: 300,
-			},
-			{
-				key: 'price_0',
-				dataIndex: 'price_0',
-				hidden: checkedList && !checkedList.includes('Предпродажа'),
-				render: (e, v) => (
-					<div className={style.price__tag}>
-						<Tag
-							color={'green'}
-							style={{
-								width: 150,
-								display: 'inline-block',
-								textAlign: 'center',
-							}}
-						>
-							Предпродажа:{' '}
-							<span style={{ background: '#fff', fontWeight: 600 }}>
-								{currentCurrency ? convertToRub(e, v.currency) : e}
-								{getCurrencySimbol(v.currency)}
-							</span>
-						</Tag>
-					</div>
-				),
-				width: 160,
-				align: 'left',
-			},
-			{
-				key: 'price_10',
-				hidden: checkedList && !checkedList.includes('Проектная'),
-				dataIndex: 'price_10',
-				render: (e, v) => (
-					<div className={style.price__tag}>
-						<Tag
-							color={'geekblue'}
-							style={{
-								width: 150,
-								display: 'inline-block',
-								textAlign: 'center',
-							}}
-						>
-							Проектная:{' '}
-							<span style={{ background: '#fff', fontWeight: 600 }}>
-								{currentCurrency ? convertToRub(e, v.currency) : e}
-								{getCurrencySimbol(v.currency)}
-							</span>
-						</Tag>
-					</div>
-				),
-				width: 160,
-				align: 'left',
-			},
-			{
-				key: 'price_20',
-				hidden: checkedList && !checkedList.includes('Прайс 20'),
-				dataIndex: 'price_20',
-				render: (e, v) => (
-					<div className={style.price__tag}>
-						<Tag
-							color={'gold'}
-							style={{
-								width: 150,
-								display: 'inline-block',
-								textAlign: 'center',
-							}}
-						>
-							Прайс 20:{' '}
-							<span style={{ background: '#fff', fontWeight: 600 }}>
-								{currentCurrency ? convertToRub(e, v.currency) : e}
-								{getCurrencySimbol(v.currency)}
-							</span>
-						</Tag>
-					</div>
-				),
-				width: 160,
-				align: 'left',
-			},
-			{
-				key: 'price_30',
-				hidden: checkedList && !checkedList.includes('Прайс 30'),
-				dataIndex: 'price_30',
-				render: (e, v) => (
-					<div className={style.price__tag}>
-						<Tag
-							color={'volcano'}
-							style={{
-								width: 150,
-								display: 'inline-block',
-								textAlign: 'center',
-							}}
-						>
-							Прайс 30:{' '}
-							<span style={{ background: '#fff', fontWeight: 600 }}>
-								{currentCurrency ? convertToRub(e, v.currency) : e}
-								{getCurrencySimbol(v.currency)}
-							</span>
-						</Tag>
-					</div>
-				),
-				width: 160,
-				align: 'left',
-			},
-		];
+	const mapDataToTreeNodes = (data, parentKey = '') => {
 		return data.map((item, index) => {
 			const key = parentKey ? `${parentKey}-${index}-${item.id}` : `${index}-${item.id}`;
 			const children = [];
 
 			if (item.models) {
 				item.models.forEach((model, mIndex) => {
+					console.log(model)
 					children.push({
 						key: `${key}-model-${mIndex}-${model.id}`,
 						title: (
-							<span>
-								<Table
-									columns={columns}
-									dataSource={[{ ...model, ...model.prices }]}
-									pagination={false}
-									size={'small'}
-									showHeader={false}
-								/>{' '}
-							</span>
+							<div className={'sa-price-option'}>
+								<div className={'sa-price-grid-cell'} style={{alignItems: 'center'}}>{model.name}</div>
+								<div className={'sa-price-grid-cell'}>{model.descr}</div>
+								<div className={'sa-price-tag'}>
+									<Tag
+										color={'green'}
+										style={{
+											width: '100%',
+											display: 'inline-block',
+											textAlign: 'center',
+											lineHeight: '27px'
+										}}
+									>
+										Розница:{' '}
+										<span style={{ background: '#fff', fontWeight: 600 }}>
+										{model.prices.price_0}
+									</span>
+									</Tag>
+								</div>
+								<div className={'sa-price-tag'}>
+									<Tag
+										color={'geekblue'}
+										style={{
+											width: '100%',
+											display: 'inline-block',
+											textAlign: 'center',
+											lineHeight: '27px'
+										}}
+									>
+										Прайс 10:{' '}
+										<span style={{ background: '#fff', fontWeight: 600 }}>
+										{model.prices.price_10}
+									</span>
+									</Tag>
+								</div>
+								<div className={'sa-price-tag'}>
+									<Tag
+										color={'gold'}
+										style={{
+											width: '100%',
+											display: 'inline-block',
+											textAlign: 'center',
+											lineHeight: '27px'
+										}}
+									>
+										Прайс 20:{' '}
+										<span style={{ background: '#fff', fontWeight: 600 }}>
+										{model.prices.price_20}
+									</span>
+									</Tag>
+								</div>
+								<div className={'sa-price-tag'}>
+									<Tag
+										color={'volcano'}
+										style={{
+											width: '100%',
+											display: 'inline-block',
+											textAlign: 'center',
+											lineHeight: '27px'
+										}}
+									>
+										Прайс 30:{' '}
+										<span style={{ background: '#fff', fontWeight: 600 }}>
+										{model.prices.price_30}
+									</span>
+									</Tag>
+								</div>
+							</div>
 						),
 						isLeaf: true,
 						dataRef: model,
@@ -191,60 +213,6 @@ const Price = () => {
 			};
 		});
 	}
-
-	useEffect(() => {
-		const get_fields = async () => {
-			if (PRODMODE) {
-				const format_data = {
-					_token: CSRF_TOKEN,
-				};
-				try {
-					setLoading(true);
-					let fields_req = await PROD_AXIOS_INSTANCE.post(`api/sales/price`, format_data);
-					if (fields_req) {
-						setData(fields_req.data.data[0].childs);
-					}
-				} catch (e) {
-					console.log(e);
-				} finally {
-					setLoading(false);
-				}
-			} else {
-				setData(PRICE.childs);
-			}
-		};
-		const get_currency = async () => {
-			if (PRODMODE) {
-				try {
-					setLoading(true);
-					const format_data = {
-						_token: CSRF_TOKEN,
-						data: {},
-					};
-					const currency_response = await PROD_AXIOS_INSTANCE.post(
-						`api/currency/getcurrency`,
-						format_data
-					);
-					if (currency_response) {
-						setCurrency(currency_response.data);
-					}
-				} catch (e) {
-					console.log(e);
-				} finally {
-					setLoading(false);
-				}
-			}
-		};
-		get_fields().then();
-		get_currency().then();
-	}, []);
-
-	useEffect(() => {
-		const treeNodes = mapDataToTreeNodes(data);
-		setTreeData(treeNodes);
-	}, [data, checkedList, currentCurrency]);
-
-	// Экспорт выбранных моделей в Excel
 	const handleExport = () => {
 		if (checkedKeys.length === 0) {
 			message.warning('Выберите хотя бы один элемент для экспорта');
@@ -293,25 +261,25 @@ const Price = () => {
 				'Предпродажа',
 				`${
 					currentCurrency ? convertToRub(m.prices.price_0, m.currency) : m.prices.price_0
-				} ${getCurrencySimbol(m.currency)}`
+				} ${getCurrencySymbol(m.currency)}`
 			);
 			addIfChecked(
 				'Проектная',
 				`${
 					currentCurrency ? convertToRub(m.prices.price_10, m.currency) : m.prices.price_10
-				} ${getCurrencySimbol(m.currency)}`
+				} ${getCurrencySymbol(m.currency)}`
 			);
 			addIfChecked(
 				'Прайс 20',
 				`${
 					currentCurrency ? convertToRub(m.prices.price_20, m.currency) : m.prices.price_20
-				} ${getCurrencySimbol(m.currency)}`
+				} ${getCurrencySymbol(m.currency)}`
 			);
 			addIfChecked(
 				'Прайс 30',
 				`${
 					currentCurrency ? convertToRub(m.prices.price_30, m.currency) : m.prices.price_30
-				} ${getCurrencySimbol(m.currency)}`
+				} ${getCurrencySymbol(m.currency)}`
 			);
 
 			return obj;
@@ -325,51 +293,91 @@ const Price = () => {
 			`${dayjs().format('DD.MM.YYYY')}. Прайс_лист. ${currentCurrency ? 'Рубли' : 'Валюта'}.xlsx`
 		);
 	};
-	const onCheckAllChange = (e) => {
-		setCheckedList(e.target.checked ? plainOptions : []);
-	};
-
-	if (loading) return <Spin tip="Загрузка прайс-листа..." />;
 
 	return (
-		<div style={{backgroundColor: 'aliceblue', width: '100%', height: 'calc(100vh - 50px)'}}>
-			<div className={style.switch__currency}>
-				<div>
-					<Checkbox indeterminate={indeterminate} onChange={onCheckAllChange} checked={checkAll}>
-						Выбрать все
-					</Checkbox>
-					<CheckboxGroup options={plainOptions} value={checkedList} onChange={onChange} />
-				</div>
-				<div style={{ marginRight: '10px' }}>
-					<Switch
-						checkedChildren="Рубли"
-						unCheckedChildren="Валюта"
-						onChange={(_) => setCurrentCurrency((prev) => !prev)}
-					/>
-				</div>
-				<div>
-					<Button onClick={handleExport} type="primary" size={'small'}>
-						Экспортировать выбранное в Excel
-					</Button>
-				</div>
+		<Spin tip="Загрузка прайс-листа..." spinning={loading}>
+			<div className={'sa-price-page'}>
+
+				<Affix offsetTop={0}>
+					<div style={{padding: '0', backgroundColor: '#b4c9e1', position: 'sticky', top: 0}}>
+						<div className={'sa-control-panel sa-flex-space sa-pa-12 sa-list-header'}>
+							<div className={'sa-header-label-container'}>
+								<div className={'sa-header-label-container-small'}>
+									<h1 className={'sa-header-label'} style={{textAlign: 'left'}}>Прайс-лист</h1>
+									<div>
+										<CurrencyMonitorBar/>
+									</div>
+								</div>
+								<div className={'sa-header-label-container-small'}>
+									<div className={'sa-vertical-flex'}>
+										<Button onClick={handleExport} type="primary">
+											Экспортировать выбранное в Excel
+										</Button>
+									</div>
+									<div style={{display: 'flex', alignItems: 'end'}}>
+										<Button
+											onClick={() => {
+												setIsOpenedFilters(!isOpenedFilters);
+											}}
+											className={`${
+												isOpenedFilters
+													? 'sa-default-solid-btn-color'
+													: 'sa-default-outlined-btn-color'
+											}`}
+											color={'default'}
+											variant={isOpenedFilters ? 'solid' : 'outlined'}
+											icon={<FilterOutlined/>}
+										>
+											Доп Фильтры
+										</Button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</Affix>
+
+
+				<Layout className={'sa-layout sa-w-100'}>
+					<Content>
+						<div className={'sa-price-tree-container'}>
+							<Tree
+								checkable
+								selectable={false}
+								treeData={treeData}
+								checkedKeys={checkedKeys}
+								onCheck={(keys) => setCheckedKeys(keys)}
+							/>
+						</div>
+					</Content>
+					<Sider
+						collapsed={!isOpenedFilters}
+						collapsedWidth={0}
+						width={'170px'}
+						style={{backgroundColor: '#ffffff', overflow: 'hidden'}}
+					>
+						<div className={'sa-sider'}>
+							{isOpenedFilters && (
+								<div className={'sa-price-sider'}>
+									{checkedList.map((option, idx) => (
+										<Checkbox onChange={(e) => onChangeCheckbox(e.target.checked, idx)}
+												  checked={option.checked}
+										>
+											{option.name}
+										</Checkbox>
+									))}
+									<Switch
+										checkedChildren="Рубли"
+										unCheckedChildren="Валюта"
+										onChange={(_) => setCurrentCurrency((prev) => !prev)}
+									/>
+								</div>
+							)}
+						</div>
+					</Sider>
+				</Layout>
 			</div>
-			<h1 style={{ marginTop: '50px' }}>Прайс-лист</h1>
-			<div className={style.price__cont}>
-				<Tree
-					checkable
-					selectable={false}
-					treeData={treeData}
-					checkedKeys={checkedKeys}
-					onCheck={(keys) => setCheckedKeys(keys)}
-					style={{
-						backgroundColor: '#b4cbe4',
-						// padding: '12px',
-						// borderRadius: '6px',
-						// border: '1px solid #d9d9d9'
-					}}
-				/>
-			</div>
-		</div>
+		</Spin>
 	);
 };
 
