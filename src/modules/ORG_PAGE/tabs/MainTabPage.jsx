eb286fe8b0f1688f220156fe44cce043604d1213
TefteleCom <vsemhoy@live.ru>
2025-10-07 15:31:32 +0300

    up

--- src/modules/ORG_PAGE/tabs/MainTabPage.jsx
+++ src/modules/ORG_PAGE/tabs/MainTabPage.jsx
@@ -83,7 +83,7 @@ const MainTabPage = (props) => {
         },
 
         label: <div className={`sa-flex-space ${item.deleted ? 'sa-orgrow-deleted' : ''}`}>
-          <div>{item.lastname ? item.lastname : ""}{item.name ? " " + item.name : ''}{item.middlename ? " " + item.middlename : ""} <span style={{ color: 'gray', fontWeight: 100 }}>({item.id})</span></div>
+          <div>{item.middlename ? item.middlename : ""}{item.name ? " " + item.name : ''}{item.lastname ? " " + item.lastname : ""} <span style={{ color: 'gray', fontWeight: 100 }}>({item.id})</span></div>
           {editMode && (
             <div className={'sa-flex-gap'}>
 
