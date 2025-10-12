063c16fb7f8f529b3a64ca6382c1af23f1d9ae66
Demian Semenov <semenovdemyan@gmail.com>
2025-10-12 18:39:42 +0300

    update

--- src/modules/1CHAT/components/ChatDivider.jsx
+++ src/modules/1CHAT/components/ChatDivider.jsx
@@ -1,11 +1,11 @@
 import { Divider } from 'antd';
 import { CalendarOutlined } from '@ant-design/icons';
 
-export const ChatDivider = ({ date }) => {
+export const ChatDivider = ({ children }) => {
 	return (
 		<Divider orientation="center" plain style={{ color: '#888' }}>
 			<CalendarOutlined style={{ marginRight: 8 }} />
-			{date}
+			{children}
 		</Divider>
 	);
 };
