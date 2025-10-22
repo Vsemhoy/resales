d13eab4dccbd708b991759a6f03918daddf6b52a
Andrew Sharanda <azzimandias@gmail.com>
2025-10-22 13:56:04 +0300

    update

--- src/modules/CHAT/components/ChatSelfMsg.jsx
+++ src/modules/CHAT/components/ChatSelfMsg.jsx
@@ -29,27 +29,27 @@ export default function ChatSelfMsg({ message }) {
 			// Изображения
 			case 'jpg': case 'jpeg': case 'png': case 'gif': case 'bmp': case 'webp': case 'svg':
 			case 'tiff': case 'ico': case 'psd': case 'ai':
-				return <FileImageOutlined />;
+				return <FileImageOutlined style={{fontSize: '25px', color: '#555555'}}/>;
 			// Документы
 			case 'pdf':
-				return <FilePdfOutlined />;
+				return <FilePdfOutlined style={{fontSize: '25px', color: '#555555'}}/>;
 			case 'doc': case 'docx': case 'rtf': case 'odt':
-				return <FileWordOutlined />;
+				return <FileWordOutlined style={{fontSize: '25px', color: '#555555'}}/>;
 			case 'xls': case 'xlsx': case 'csv': case 'ods':
-				return <FileExcelOutlined />;
+				return <FileExcelOutlined style={{fontSize: '25px', color: '#555555'}}/>;
 			case 'ppt': case 'pptx': case 'odp':
-				return <FilePptOutlined />; // или FileOutlined
+				return <FilePptOutlined style={{fontSize: '25px', color: '#555555'}}/>;
 			case 'md': case 'markdown':
-				return <FileMarkdownOutlined />;
+				return <FileMarkdownOutlined style={{fontSize: '25px', color: '#555555'}}/>;
 			// Архивы
 			case 'zip': case 'rar': case '7z': case 'tar': case 'gz':
-				return <FileZipOutlined />;
+				return <FileZipOutlined style={{fontSize: '25px', color: '#555555'}}/>;
 			// Текстовые файлы
 			case 'txt': case 'log': case 'ini': case 'xml': case 'json': case 'yaml':
-				return <FileTextOutlined />;
+				return <FileTextOutlined style={{fontSize: '25px', color: '#555555'}}/>;
 			// Остальное - общая иконка
 			default:
-				return <FileOutlined />;
+				return <FileOutlined style={{fontSize: '25px', color: '#555555'}}/>;
 		}
 	};
 
@@ -58,24 +58,27 @@ export default function ChatSelfMsg({ message }) {
 			<div className={bubbleClass}>
 				<div className={styles.senderName}><span style={{color: 'red'}}>{fromId}</span> {senderName}</div>
 				<span>{text}</span>
-				<div className={styles.time} style={{display: 'flex', justifyContent: 'flex-end', height: '18px'}} >
+				<div className={styles.files_container}>
+					{files.length > 0 && files.map((file, index) => (
+						<a href={`${HTTP_HOST}/${file.route}`} target={'_blank'} className={styles.file}>
+							<div className={styles.file_circle}>
+								{pasteFileIcon(file.extension)}
+							</div>
+							<p className={styles.href_label}>{file.route}</p>
+						</a>
+					))}
+				</div>
+				<div className={styles.time} style={{display: 'flex', justifyContent: 'flex-end', height: '18px'}}>
 					<div>{dayjs(+timestamp * 1000).format('HH:mm')}</div>
 					{!isSending &&
 						<span style={{width: '18px', height: '18px', position: 'relative'}}
 							  title={read ? 'Прочитано' : 'Отправлено'}
 						>
 							<CheckOutlined style={{color: read ? 'green' : ''}}/>
-							{read && <CheckOutlined style={{position: 'absolute', top: '4px', left: '3px', color: read ? 'green' : ''}}/>}
+							{read && <CheckOutlined
+								style={{position: 'absolute', top: '4px', left: '3px', color: read ? 'green' : ''}}/>}
 						</span>
 					}
-				</div>
-				<div className={styles.files_container}>
-					{files.length > 0 && files.map((file, index) => (
-						<div className={styles.file}>
-							{pasteFileIcon(file.extension)}
-							<a href={`${HTTP_HOST}/${file.route}`}>file.route</a>
-						</div>
-					))}
 				</div>
 				{isSending && <span className={styles.sending}> • отправляется...</span>}
 			</div>
