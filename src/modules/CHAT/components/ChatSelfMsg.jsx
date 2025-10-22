8078ddb880b50384d9f8cf62d0a092562272228d
Andrew Sharanda <azzimandias@gmail.com>
2025-10-22 13:10:05 +0300

    update

--- src/modules/CHAT/components/ChatSelfMsg.jsx
+++ src/modules/CHAT/components/ChatSelfMsg.jsx
@@ -1,9 +1,21 @@
 import styles from './style/Chat.module.css';
 import dayjs from 'dayjs';
-import {CheckOutlined} from "@ant-design/icons";
+import {
+	CheckOutlined, FileExcelOutlined,
+	FileGifOutlined,
+	FileImageOutlined,
+	FileJpgOutlined,
+	FileMarkdownOutlined,
+	FileOutlined,
+	FilePdfOutlined, FilePptOutlined,
+	FileTextOutlined,
+	FileWordOutlined,
+	FileZipOutlined
+} from "@ant-design/icons";
+import {HTTP_HOST} from "../../../config/config";
 
 export default function ChatSelfMsg({ message }) {
-	const { text, timestamp, isSending, senderName, fromId, read } = message;
+	const { text, files, timestamp, isSending, senderName, fromId, read } = message;
 
 	// Используем только существующие классы
 	const messageClass = `${styles.message} ${styles.myMessage} ${
@@ -11,6 +23,35 @@ export default function ChatSelfMsg({ message }) {
 	}`;
 
 	const bubbleClass = `${styles.bubble} ${styles.myMessageBubble}`;
+
+	const pasteFileIcon = (extension) => {
+		switch (extension) {
+			// Изображения
+			case 'jpg': case 'jpeg': case 'png': case 'gif': case 'bmp': case 'webp': case 'svg':
+			case 'tiff': case 'ico': case 'psd': case 'ai':
+				return <FileImageOutlined />;
+			// Документы
+			case 'pdf':
+				return <FilePdfOutlined />;
+			case 'doc': case 'docx': case 'rtf': case 'odt':
+				return <FileWordOutlined />;
+			case 'xls': case 'xlsx': case 'csv': case 'ods':
+				return <FileExcelOutlined />;
+			case 'ppt': case 'pptx': case 'odp':
+				return <FilePptOutlined />; // или FileOutlined
+			case 'md': case 'markdown':
+				return <FileMarkdownOutlined />;
+			// Архивы
+			case 'zip': case 'rar': case '7z': case 'tar': case 'gz':
+				return <FileZipOutlined />;
+			// Текстовые файлы
+			case 'txt': case 'log': case 'ini': case 'xml': case 'json': case 'yaml':
+				return <FileTextOutlined />;
+			// Остальное - общая иконка
+			default:
+				return <FileOutlined />;
+		}
+	};
 
 	return (
 		<div className={messageClass}>
@@ -27,6 +68,14 @@ export default function ChatSelfMsg({ message }) {
 							{read && <CheckOutlined style={{position: 'absolute', top: '4px', left: '3px', color: read ? 'green' : ''}}/>}
 						</span>
 					}
+				</div>
+				<div className={styles.files_container}>
+					{files.length > 0 && files.map((file, index) => (
+						<div className={styles.file}>
+							{pasteFileIcon(file.extension)}
+							<a href={`${HTTP_HOST}/${file.route}`}>file.route</a>
+						</div>
+					))}
 				</div>
 				{isSending && <span className={styles.sending}> • отправляется...</span>}
 			</div>
