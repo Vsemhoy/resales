b983792bd52aa96cb963f9f21f1702bf728069f0
Andrew Sharanda <azzimandias@gmail.com>
2025-11-18 12:37:21 +0300

    update

--- src/modules/NOTIFIER/NotiBtn.jsx
+++ src/modules/NOTIFIER/NotiBtn.jsx
@@ -3,28 +3,10 @@ import {NotificationOutlined} from "@ant-design/icons";
 import {Button, Space} from "antd";
 import styles from "../CHAT/components/style/Chat.module.css";
 import NotifierDrawer from "./NotifierDrawer";
-import {useChatSocket} from "../../context/ChatSocketContext";
 
 const NotiBtn = () => {
     const [totalUnread, setTotalUnread] = useState(0);
     const [isOpenDrawer, setIsOpenDrawer] = useState(false);
-
-    const {
-        connected,           // boolean - подключен ли WebSocket
-        connectionStatus,    // string - статус подключения
-        chatsList,           // [] - список чатов
-        messages,            // {} - сообщения по chatId
-        joinRoom,            // function - войти в комнату
-        sendMessage,         // function - отправить сообщение
-        on, off,             // function - подписка на события
-        updateMessage,       // function - обновить сообщение
-        replyToMessage,      // function - ответить на сообщение
-        editMessage,         // function - редактировать сообщение
-        deleteMessage,       // function - удалить сообщение
-        updateMessageStatus, // function - обновить статус
-        fetchChatsList,
-
-    } = useChatSocket();
 
     const openDrawer = () => {
         setIsOpenDrawer(true);
