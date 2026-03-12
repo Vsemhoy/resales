74d6129b40146dc89f87681bfed40c04828b6330
Andrew Sharanda <azzimandias@gmail.com>
2026-03-12 12:48:02 +0300

    update

--- src/config/Api.js
+++ src/config/Api.js
@@ -3,14 +3,7 @@ import {HTTP_ROOT, HTTP_HOST} from './config';
 // const PROD_API_URL = 'http://89.104.68.50'
 // export const PROD_API_URL = 'https://pulse-retail.ru'
 axios.defaults.withCredentials = true;
-//const token = document.querySelector('meta[name="jwt"]')?.getAttribute('content');
 export const PROD_AXIOS_INSTANCE = axios.create({
     baseURL: HTTP_ROOT,
     timeout: 300000,
 });
-/*PROD_AXIOS_INSTANCE.interceptors.request.use((config) => {
-    if (token) {
-        config.headers['Authorization'] = `Bearer ${token}`;
-    }
-    return config;
-});*/
