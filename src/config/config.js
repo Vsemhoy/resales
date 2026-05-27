892d27c6f67e13d9b6c320aeac569d4f9d6992bd
TefteleCom <77360455+Vsemhoy@users.noreply.github.com>
2026-05-27 15:47:10 +0300

    Fix duplicating of Calls to future

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,4 @@
-export const PRODMODE = !(
-	window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
-);
+export const PRODMODE = true;
 // Websocket backend-for-frontend PORT
 export const BFF_PORT = 5003;
 
